08c1335fe80c83b21a2b0a9d1848b93895c6fecc
Riccardo <riccardo960@gmail.com>
2023-09-27 20:04:58 +0200

    added pool of threads

--- 3/mainParallel.js
+++ 3/mainParallel.js
@@ -34,30 +34,45 @@ const workerHandler = (n) => {
 if (isMainThread) {
   const n = 10,
     m = parseInt(process.argv[2], 10) || 100,
+    poolSize = 4,
+    pool = Array(poolSize)
+      .fill(0)
+      .map(() => new Worker(__filename, { workerData: { n } })),
     sharedArray = new SharedArrayBuffer(Float32Array.BYTES_PER_ELEMENT * n),
-    target = new Float32Array(sharedArray),
-    worker = new Worker(__filename, { workerData: { n } });
+    target = new Float32Array(sharedArray);
+
   for (let i = 0; i < n; i++) {
     target[i] = Math.random();
   }
-  worker.postMessage({ target });
+  pool.forEach((worker) => worker.postMessage({ target }));
 
   for (let i = 0; i < m; i++) {
     const array = createArray(n);
-    worker.postMessage({ vector: { id: `${i}`, array } }, [array.buffer]);
+
+    pool[i % poolSize].postMessage({ vector: { id: `${i}`, array } }, [
+      array.buffer,
+    ]);
   }
 
   const result = [];
-  worker
-    .on("online", () => console.log("online"))
-    .on("exit", (exitCode) => {
-      console.log(`exit code ${exitCode}`);
-      console.log("total = ", result.length);
-      console.table(result.slice(0, 10));
-    })
-    .on("message", (msg) => result.push(...msg))
-    .on("error", console.error);
-  worker.postMessage({ compute: true });
+  let threadCount = 0;
+  pool.forEach((worker) => {
+    worker
+      .on("online", () => console.log("online"))
+      .on("exit", (exitCode) => {
+        console.log(`exit code ${exitCode}`);
+        threadCount++;
+        if (threadCount === poolSize) {
+          result.sort((a, b) => b.score - a.score);
+          console.table(result.slice(0, 20));
+          console.log("total = ", result.length);
+        }
+      })
+      .on("message", (msg) => result.push(...msg))
+      .on("error", console.error);
+
+    worker.postMessage({ compute: true });
+  });
 } else {
   parentPort.on("message", workerHandler(workerData.n));
 }
