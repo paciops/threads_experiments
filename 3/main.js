6efd86e830613e6c9532949529828a3e8c4f8407
Riccardo <riccardo960@gmail.com>
2023-09-27 20:00:37 +0200

    log total vector count

--- 3/main.js
+++ 3/main.js
@@ -2,7 +2,7 @@ const { findSimilarVectors, getMagnitude } = require("./vector");
 
 const createArray = (n) => new Float32Array(Array(n).fill(0).map(Math.random));
 
-function main(n = 10, m = 50_000) {
+function main(n = 10, m = parseInt(process.argv[2], 10) || 100) {
   const target = createArray(n);
   const vectors = Array(m)
     .fill(0)
@@ -15,9 +15,8 @@ function main(n = 10, m = 50_000) {
       {}
     );
 
-  console.table(getMagnitude(target, n));
-
-  console.log(findSimilarVectors(target, vectors, n, 0.9).length);
+  const similarArrays = findSimilarVectors(target, vectors, n);
+  console.log({ vectors: m, similar: similarArrays.length });
 }
 
 main();
